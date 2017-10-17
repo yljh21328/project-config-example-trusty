a44b9d7cc2f977c3ff84e5313e8e8f532cf45f74
qnap-ci <openstack-ci@qnap.com>
2017-10-17 17:01:53 +0800

    Update nodepool, layout of zuul and add custom script for nodepool.

---
+++ nodepool/scripts/custom.sh
@@ -0,0 +1,5 @@
+# Make sure our indexes are up to date.
+sudo add-apt-repository ppa:openjdk-r/ppa -y
+sudo apt-get update
+sudo apt-get install -y openjdk-8-jdk
+sudo apt-get install -y bridge-utils
